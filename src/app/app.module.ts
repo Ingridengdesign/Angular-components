227beed1b4b94fe0fbfe6d4b383b8065794e9b8e
Ingrid Emanuelle Sousa Dias <ingrid.dias@nav.com.br>
2022-03-29 17:09:07 -0300

    exercicio botao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./AppComponent";
+import { PrimeiroComponent } from './primeiro/primeiro.component';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponent,
+    MeuPrimeiroComponent
+
   ],
   imports: [
     BrowserModule,
